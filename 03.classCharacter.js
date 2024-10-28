d0546563c92658f15db6f32746205669d38e467e
Akashkr28 <email.akash2010@gmail.com>
2024-10-28 20:07:28 +0530

    Added output as comment for every console.log

--- 03.classCharacter.js
+++ 03.classCharacter.js
@@ -7,23 +7,34 @@ function findMatches(inputString, pattern) {
 const testString = "Hello World! 1234 @JavaScript#";
 
 console.log("Input String: ", testString);
+// Output : Input String:  Hello World! 1234 @JavaScript#
+
 
 // Find digits
 const digitPattern = '\\d'; // Character class for digits
 const digitMatches = findMatches(testString, digitPattern);
 console.log("Digits: ", digitMatches);
+// Output : Digits:  [ '1', '2', '3', '4' ]
 
 // Find uppercase letters
 const uppercasePattern = '[A-Z]'; // Character class for uppercase letters
 const uppercaseMatches = findMatches(testString, uppercasePattern);
 console.log("Uppercase Letters: ", uppercaseMatches);
+// Output : Uppercase Letters:  [ 'H', 'W', 'J', 'S' ]
 
 // Find lowercase letters
 const lowercasePattern = '[a-z]'; // Character class for lowercase letters
 const lowercaseMatches = findMatches(testString, lowercasePattern);
 console.log("Lowercase Letters: ", lowercaseMatches);
+// Output: Lowercase Letters:  [
+//   'e', 'l', 'l', 'o',
+//   'o', 'r', 'l', 'd',
+//   'a', 'v', 'a', 'c',
+//   'r', 'i', 'p', 't'
+// ]
 
 // Find special characters
 const specialCharPattern = '[^a-zA-Z0-9]'; // Character class for special characters (not letters or digits)
 const specialCharMatches = findMatches(testString, specialCharPattern);
 console.log("Special Characters: ", specialCharMatches);
+// Output : Special Characters:  [ ' ', '!', ' ', ' ', '@', '#' ]
